6b3edf17ae774fd920692309d2b7753d09e47c8a
ajfm88 <ajfoucaultmo@mail.usf.edu>
2023-08-27 22:00:16 -0700

    Section 05: Problem Solving Patterns: 29. Frequency Counter: Anagram Challenge. Coding Exercise 1: Frequency Counter - validAnagram. 30. Anagram Challenge Solution.

---
+++ 05-problem-solving-patterns/anagram-challenge.js
@@ -0,0 +1,23 @@
+/*
+Frequency Counter - validAnagram
+Given two strings, write a function to determine if the second string is an anagram of the first. 
+An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+Examples:
+
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram("rat","car") // false) // false
+validAnagram('awesome', 'awesom') // false
+validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+validAnagram('qwerty', 'qeywrt') // true
+validAnagram('texttwisttime', 'timetwisttext') // true
+Note: You may assume the string contains only lowercase alphabets.
+
+Time Complexity - O(n)
+*/
+
+function validAnagram() {
+  // add whatever parameters you deem necessary - good luck!
+}
