e671f00eac6054b97797dd05c833a8d14a7fd2b0
ajfm88 <ajfoucaultmo@mail.usf.edu>
2023-08-28 16:07:15 -0700

    Section 05: Problem Solving Patterns: My attempt of the Frequency Counter: Anagram Challenge.

--- 05-problem-solving-patterns/anagram-challenge.js
+++ 05-problem-solving-patterns/anagram-challenge.js
@@ -8,7 +8,7 @@ Examples:
 validAnagram('', '') // true
 validAnagram('aaz', 'zza') // false
 validAnagram('anagram', 'nagaram') // true
-validAnagram("rat","car") // false) // false
+validAnagram("rat","car") // false
 validAnagram('awesome', 'awesom') // false
 validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
 validAnagram('qwerty', 'qeywrt') // true
@@ -18,6 +18,35 @@ Note: You may assume the string contains only lowercase alphabets.
 Time Complexity - O(n)
 */
 
-function validAnagram() {
-  // add whatever parameters you deem necessary - good luck!
+function validAnagram(arr1, arr2) {
+  // first we see if they have the same length
+  if (arr1.length !== arr2.length) return false;
+
+  // second, we create objects to hold the values of the frequencies of the arrays
+  let anagram1 = {};
+  let anagram2 = {};
+
+  // third, we populate these objects with each element of the arrays and its frequency
+  // key = element (letter) of the array
+  // value = how often the element (letter) appears in the array
+  for (let elem of arr1) {
+    anagram1[elem] = (anagram1[elem] || 0) + 1;
+  }
+  for (let elem of arr1) {
+    anagram2[elem] = (anagram2[elem] || 0) + 1;
+  }
+
+  // fourth, iterate through each key and value pair in each object
+  // make sure they match in value and in number
+  for (let key in anagram1) {
+    if (!anagram2[key] || anagram1[key] !== anagram2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
+// Example usage
+console.log(validAnagram("anagram", "nagaram")); // Output: true
+console.log(validAnagram("rat", "car")); // Output: false but it is still giving me true
