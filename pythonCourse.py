8edb2bebe77a40ea509da539e5025de194724dd6
ajfm88 <ale@ajfm88.com>
2025-03-17 16:27:22 -0700

    Python for Beginners - Full Course: 01. Intro. 02. Hello World. 03. What is Python? 04. Execution Order. 05. Printing Text. 06. Code Errors. 07. Comments. 08. Variable Declaration.

---
+++ pythonCourse.py
@@ -0,0 +1,16 @@
+# Hello World.
+print("Hello World")
+
+# Printing Text.
+print("My favorite quote is \"To be or not to be.\"")
+print('My favorite quote is "To be or not to be."')
+
+# Code Errors.
+print("Can someone please add a closing parenthesis?")
+
+# Comments.
+
+# Variable Declaration.
+message = "This string is stored in a variable."
+print(message)
+print(message)
