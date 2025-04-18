d8846af20b37cea34bb5471341ec8202dbccc710
ajfm88 <ale@ajfm88.com>
2025-04-18 10:02:43 -0700

    Python for Beginners - Full Course: 77. Reading Input. 78. Type Conversion with Input. 79. Parse Input. 80. Try Except. 81. Error Catching. 82. Multiple Except Blocks.

--- pythonCourse.py
+++ pythonCourse.py
@@ -862,3 +862,61 @@ my_dict = {"a":1, "b":2, "c":3}
 value = list(my_dict.values())
 
 print(values)       # Output: [1, 2, 3]
+
+
+
+# Reading Input
+user_input = input("Enter some text: ")
+print(user_input)
+
+
+
+# Type Conversion with Input
+# By default, input() returns the user input as a string.
+# You need to explicitly convert it if you need something else.
+age = int(input("Enter your age: "))
+
+temperature = (float(input("Enter the temperature: ")))
+
+
+
+# Parse Input
+# Restructuring the input in some way is called PARSING the input
+number_string = "1,2,3"
+
+string_list = number_list.split(",")
+
+print(string_list)      # Output: ['1', '2', '3']
+
+
+
+# Try Except
+try:
+    # Code that might cause an erro
+    result = 10 / 0
+except:
+    print("An error occured!")
+
+print("This will be printed regardless")
+
+
+
+# Error Catching
+try:
+    result = 10 / 0
+except Exception as error:
+    print("Error:", error)
+
+
+
+# Multiple Except Blocks
+try:
+    num1 = int(a)
+    num2 = int(b)
+    result = num1 / num2
+except ValueError:
+    print("Error: Invalid value!")
+except ZeroDivisionError:
+    print("Error: Division by zero!")
+except Exception as error:
+    print("An error occurred: ", error)
