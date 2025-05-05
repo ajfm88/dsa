7afdce467a5db53907e69a17d1cc09f0255c7eca
ajfm88 <ale@ajfm88.com>
2025-05-05 11:46:11 -0700

    Leetcode Problem # 001 - Two Sum | Hashmap implementation.

--- 01-two-sum.py
+++ 01-two-sum.py
@@ -33,7 +33,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j] ==target):
+                if (nums[i] + nums[j] == target):
                     return [i, j]
 
 '''
@@ -45,3 +45,33 @@ n * (n/2)
 (n^2)/2
 O(n^2)
 '''
+
+
+'''Hashmap implementation 
+
+[3, 9, 5, 2, 6, 8, 0] => target = 10
+          i
+
+ {3: 0, 9: 1, 5: 2, 2: 3, 6: 4, 8: 5}
+
+ target - i = j
+ 10     - 8 = 2
+
+ 1. Iterate through array.
+ 2. Check to see whether hash table contains: target - i
+ 3. If not, insert i into hash table as key, and i's index as value
+ 4. If target - i IS inside the hash table, we return index of i, 
+    value associated with key in the hash table
+
+Lookup time in Big O notation: O(N)
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for index, value in enumerate(nums):
+            if target - value in hash_table:
+                return [hash_table[target - value], index]
+            else:
+                hash_table[value] = index
+        return None
