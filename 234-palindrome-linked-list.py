b0760c74785855b7dc2859d1fc6c9d079f76ea74
ajfm88 <ale@ajfm88.com>
2025-07-10 07:32:22 -0700

    Leetcode Problem #234 — Palindrome Linked List | NeetCode’s optimal solution.

--- 234-palindrome-linked-list.py
+++ 234-palindrome-linked-list.py
@@ -89,3 +89,38 @@ class Solution:
             l += 1
             r -= 1
         return True
+    
+'''
+NeetCode Optimal Solution
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+
+        # find middle (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # reverse second half
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        # check palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
