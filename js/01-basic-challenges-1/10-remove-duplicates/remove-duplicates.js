c2bd2db0e024a1c6689b4784f767ca63e4f3f5c6
ajfm88 <ale@ajfm88.com>
2025-11-10 23:18:13 -0800

    15. Remove Duplicates.

---
+++ js/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -0,0 +1,17 @@
+// function removeDuplicates(arr) {
+//   const uniqueArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!uniqueArr.includes(arr[i])) {
+//       uniqueArr.push(arr[i]);
+//     }
+//   }
+
+//   return uniqueArr;
+// }
+
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
+
+module.exports = removeDuplicates;
