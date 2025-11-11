d6583de0393eaa923d85025c8c3e4caa84120ed2
ajfm88 <ale@ajfm88.com>
2025-11-10 21:12:47 -0800

    13. Palindrome.

---
+++ js/01-basic-challenges-1/08-palindrome/palindrome-run.js
@@ -0,0 +1,8 @@
+const isPalindrome = require("./palindrome");
+
+const result1 = isPalindrome("racecar");
+const result2 = isPalindrome("madam");
+const result3 = isPalindrome("racecars");
+const result4 = isPalindrome("madams");
+
+console.log(result1, result2, result3, result4);
