599e82074a73ed151af33047fec2b6c91f171443
ajfm88 <ale@ajfm88.com>
2025-11-22 09:52:43 -0800

    24. Format Phone Number.

---
+++ js/02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -0,0 +1,22 @@
+// function formatPhoneNumber(numbers) {
+//   const areaCode = numbers.slice(0, 3).join("");
+//   const prefix = numbers.slice(3, 6).join("");
+//   const lineNumber = numbers.slice(6).join("");
+
+//   return `(${areaCode}) ${prefix}-${lineNumber}`;
+// }
+
+// function formatPhoneNumber(numbers) {
+//   const formatted = numbers.join("");
+//   return `(${formatted.substring(0, 3)}) ${formatted.substring(
+//     3,
+//     6
+//   )}-${formatted.substring(6)}`;
+// }
+
+const formatPhoneNumber = (numbers) =>
+  `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers
+    .slice(6)
+    .join("")}`;
+
+module.exports = formatPhoneNumber;
