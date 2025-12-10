f293ad31b4cea328726b9c973ba0c0ab86b57bb7
ajfm88 <ale@ajfm88.com>
2025-12-10 11:49:56 -0800

    30. Valid Anagrams.

---
+++ js/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -0,0 +1,17 @@
+function validAnagrams(str1, str2) {
+  const freqCount1 = str1.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  const freqCount2 = str2.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freqCount1).every(
+    (char) => freqCount1[char] === freqCount2[char]
+  );
+}
+
+module.exports = validAnagrams;
