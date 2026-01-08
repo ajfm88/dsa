58c7f6beece10746e141a99c60fe5007ec91bdbc
ajfm88 <ale@ajfm88.com>
2026-01-08 15:14:06 -0800

    55. Max Subarray - Linear.

---
+++ js/05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -0,0 +1,21 @@
+function maxSubarraySum(arr, k) {
+  let currentSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
+
+  currentSum = maxSum;
+
+  for (let i = k; i < arr.length; i++) {
+    currentSum = currentSum - arr[i - k] + arr[i];
+    console.log(currentSum);
+
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+module.exports = maxSubarraySum;
