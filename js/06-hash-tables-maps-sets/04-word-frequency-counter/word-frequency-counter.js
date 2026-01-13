9b5dcf3029c78d087d97ef081d2dcef66719e15d
ajfm88 <ale@ajfm88.com>
2026-01-13 10:09:01 -0800

    58. Word Frequency Counter.

---
+++ js/06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -0,0 +1,19 @@
+function wordFrequencyCounter(str) {
+  const words = str.toLowerCase().split(/\W+/);
+
+  const wordFrequency = new Map();
+
+  for (const word of words) {
+    if (word === "") continue;
+
+    if (wordFrequency.has(word)) {
+      wordFrequency.set(word, wordFrequency.get(word) + 1);
+    } else {
+      wordFrequency.set(word, 1);
+    }
+  }
+
+  return wordFrequency;
+}
+
+module.exports = wordFrequencyCounter;
