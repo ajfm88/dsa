df65e25e2844f1559fb8ee4183769c63c2095831
ajfm88 <ale@ajfm88.com>
2026-01-15 15:11:28 -0800

    60. Anagram Grouping.

---
+++ js/06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -0,0 +1,17 @@
+function anagramGrouping(words) {
+  const anagramGroups = new Map();
+
+  for (const word of words) {
+    const sortedChars = word.split("").sort().join("");
+
+    if (anagramGroups.has(sortedChars)) {
+      anagramGroups.get(sortedChars).push(word);
+    } else {
+      anagramGroups.set(sortedChars, [word]);
+    }
+  }
+
+  return Array.from(anagramGroups.values());
+}
+
+module.exports = anagramGrouping;
