6c46d2ed6a24f1ca10a7ca16cc46476fe10abafc
ａｊｆｍ８８ <ale@ajfm88.com>
2026-01-20 10:57:10 -0800

    63. Two Sum.

---
+++ js/06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -0,0 +1,17 @@
+function twoSum(nums, target) {
+  const numSet = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (numSet.has(complement)) {
+      return [nums.indexOf(complement), i];
+    }
+
+    numSet.add(nums[i]);
+  }
+
+  return [];
+}
+
+module.exports = twoSum;
