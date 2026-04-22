c270ff0a6f493b64974f3d26c6f5095c55413554
ａｊｆｍ８８ <ale@ajfm88.com>
2026-04-22 11:33:04 -0700

    109. Selection Sort Implementation.

---
+++ js/09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+module.exports = selectionSort;
